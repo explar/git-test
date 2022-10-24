bf7cf0fb3cadf30fd0dcc4bb0780f5eff91b3f3b
wzh <1273246533@qq.com>
2022-10-24 16:25:14 +0800

    hot-fix commit1

--- src/main/java/com/wzh/git/GitTest.java
+++ src/main/java/com/wzh/git/GitTest.java
@@ -6,8 +6,9 @@ package com.wzh.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello Git!!");
-        System.out.println("hello Git!!");
-        System.out.println("hello Git!!");
+        System.out.println("hello Git!!1");
+        System.out.println("hello Git!!2");
+        System.out.println("hello Git!!3");
+        System.out.println("hello Git!!4");
     }
 }
