ef0e931c69e077fda91d58b6a6c888c631e2e4c3
wzh <1273246533@qq.com>
2022-10-24 09:40:39 +0800

    first commit

---
+++ src/main/java/com/wzh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzh.git;
+
+/**
+ * @author Mr Wang
+ * @create 2022/10/24 9:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!");
+    }
+}
